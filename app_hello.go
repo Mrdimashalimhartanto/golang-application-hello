12c484a81b842a22873bad0d1c62ab9a3e5652bd
Dimas Halim Hartanto <105198462+samidhalim@users.noreply.github.com>
2025-10-22 19:37:46 +0700

    update create new function AlamatTinggal

--- app_hello.go
+++ app_hello.go
@@ -3,3 +3,7 @@ package golang_application_hello
 func SayHelloGuys(name string) string {
 	return "hallo bro" + name
 }
+
+func AlamatTinggal(address string) string {
+	return "Address" + address
+}
